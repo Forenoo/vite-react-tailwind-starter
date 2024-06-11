609bef0760e7b1d3d45a6019cfb2b01354275aac
forenoo <forenofaisal21@gmail.com>
2024-06-11 17:27:51 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center bg-gray-900 justify-center">
+      <code className="font-semibold text-[2rem] bg-white/20 px-4 rounded-xl text-white">
+        Starter Vite + React + TailwindCSS
+      </code>
+    </div>
+  );
+};
+
+export default App;
